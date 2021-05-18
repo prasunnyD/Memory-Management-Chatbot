20e87eeb1440b82ba2c09f3b3dffa4bf3f87d955
root <prasunnyD@github.com>
2021-05-18 22:15:23 +0000

    more task 3 updates to graphnode and chatlogic

--- src/graphnode.h
+++ src/graphnode.h
@@ -18,10 +18,10 @@ private:
     ////
 
     // data handles (owned)
-    std::vector<GraphEdge *> _childEdges;  // edges to subsequent nodes
+    std::vector<std::unique_ptr<GraphEdge>> _childEdges;  // edges to subsequent nodes
 
     // data handles (not owned)
-    std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
+    std::vector<std::unique_ptr<GraphEdge>> _parentEdges; // edges to preceding nodes 
     ChatBot *_chatBot;
 
     ////
